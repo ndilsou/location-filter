809e73876cba51aca09e09f91d1086538f2fe9d3
Ndil-Adjim Sou Ngadoy <nasoungadoy@gmail.com>
2018-06-28 19:46:40 +0100

    started investigation of the api

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,12 +5,12 @@ apt-get -qqy install python3 python3-pip
 apt-get -qqy install git python-pip make build-essential libssl-dev zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev curl
 
 git clone https://github.com/yyuu/pyenv.git ~/.pyenv
-git clone https://github.com/yyuu/pyenv-virtualenvwrapper.git ~/.pyenv/plugins/pyenv-virtualenvwrapper
+
 
 echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
-echo 'pyenv virtualenvwrapper' >> ~/.bashrc
+
 
 apt-get -qqy install apt-transport-https ca-certificates curl software-properties-common
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
