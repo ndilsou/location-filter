aea04936d884e5648a38190b38fe9d1cc97e2f7b
Ndil-Adjim Sou Ngadoy <nasoungadoy@gmail.com>
2018-06-29 18:36:29 +0100

    adding all Item representations for spyder

--- annotation_filter/annotation_filter/items.py
+++ annotation_filter/annotation_filter/items.py
@@ -53,8 +53,79 @@ class ArticleItem(Item):
 
 
 class AnnotationItem(Item):
-    pass
+    iid = Field()
+    label = Field()
+    origin_types = Field()
+    uri = Field()
+    lastModifiedTime = Field()
+    publishedTime = Field()
+    alternateLabels = Field()
+    id = Field()
+    type = Field()
+    title = Field()
+    lod = Field()
+    abstract = Field()
+    categories = Field()
+
+    @classmethod
+    def from_dict(cls, data):
+        item = cls()
+        fill_field('iid', data, item)
+        fill_field('label', data, item)
+        fill_field('origin_types', data, item)
+        fill_field('uri', data, item)
+        fill_field('lastModifiedTime', data, item)
+        fill_field('publishedTime', data, item)
+        fill_field('alternateLabels', data, item)
+        fill_field('type', data, item)
+        fill_field('title', data, item)
+        fill_field('lod', data["item"], item)
+        fill_field('abstract', data["item"], item)
+        fill_field('categories', data["item"], item)
+        return item
+
+    def __repr__(self):
+        return f"Annotation{{ title: {self['title']} }}"
 
 
 class LocationItem(Item):
-    pass
+    iid = Field()
+    council_annotation_uri = Field()
+    county = Field()
+    local_government_area = Field()
+    nuts_region = Field()
+    lng = Field()
+    lat = Field()
+    country = Field()
+    name = Field()
+    lastModifiedTime = Field()
+    origin_type = Field()
+    annotation_uri = Field()
+    id = Field()
+    type = Field()
+    postcode_sector = Field()
+
+    @classmethod
+    def from_dict(cls, data):
+        item = cls()
+        fill_field('iid', data, item)
+        fill_field('council_annotation_uri', data, item)
+        fill_field('county', data, item)
+        fill_field('local_government_area', data, item)
+        fill_field('nuts_region', data, item)
+        fill_field('lng', data, item)
+        fill_field('lat', data, item)
+        fill_field('country', data, item)
+        fill_field('name', data, item)
+        fill_field('lastModifiedTime', data, item)
+        fill_field('origin_type', data, item)
+        fill_field('annotation_uri', data, item)
+        fill_field('id', data, item)
+        fill_field('type', data, item)
+        fill_field('postcode_sector', data, item)
+
+
+        return item
+
+    def __repr__(self):
+        return f"Location{{ title: {self['name']}, loc:[{self['lng']},{self['lat']}]}}"
--- annotation_filter/annotation_filter/spiders/article_spider.py
+++ annotation_filter/annotation_filter/spiders/article_spider.py
@@ -1,6 +1,8 @@
 import scrapy
 import json
-from annotation_filter.items import ArticleItem
+from annotation_filter.items import ArticleItem, AnnotationItem, LocationItem
+
+DEBUG_LIMIT = 5
 
 
 class ArticleSpider(scrapy.Spider):
@@ -12,6 +14,7 @@ class ArticleSpider(scrapy.Spider):
 
     def parse(self, response):
         json_response = json.loads(response.text)
+        DEBUG_I = 0
         for json_article in json_response:
             article = ArticleItem.from_dict(json_article)
             self.log(article)
@@ -19,10 +22,19 @@ class ArticleSpider(scrapy.Spider):
                 yield response.follow(f"{self.annotation_url}?uri={annotation_uri}", self.parse_annotations)
                 yield response.follow(f"{self.location_url}?annotation_uri={annotation_uri}", self.parse_locations)
 
+            DEBUG_I += 1
+            if DEBUG_I >= DEBUG_LIMIT:
+                break
+
     def parse_annotations(self, response):
         json_response = json.loads(response.text)
-        self.log(f"Annotation{{ {json_response} }}")
+        for json_annotation in json_response:
+            annotation = AnnotationItem.from_dict(json_annotation)
+            self.log(annotation)
 
     def parse_locations(self, response):
         json_response = json.loads(response.text)
-        self.log(f"Location{{ {json_response} }}")
+        json_response = json.loads(response.text)
+        for json_location in json_response:
+            location = LocationItem.from_dict(json_location)
+            self.log(location)
